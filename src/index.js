d5e8a61a8151d077bdee1291b9fe4d07b925a7a8
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-03-05 23:49:34 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './i18n';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./styles/global.css"; 
+
+ReactDOM.render(<App />, document.getElementById("root"));
